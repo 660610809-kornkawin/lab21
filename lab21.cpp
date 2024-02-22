e4054bcda3376b0d86dc111473af98dec745249b
660610809-kornkawin <152590724+660610809-kornkawin@users.noreply.github.com>
2024-02-22 20:42:27 +0700

    completed2

--- lab21.cpp
+++ lab21.cpp
@@ -4,184 +4,9 @@
 #include<cstdlib>
 #include<vector>
 #include<iomanip>
+#include "lab21.h"
 
 using namespace std;
-
-class Equipment{
-	int hpmax
-	int atk;
-	int def;a
-	public:
-		Equipment(int,int,int);
-		vector<int> getStat();			
-};
-
-class Unit{
-		string name;
-		string type;		
-		int hp;
-		int hpmax;
-		int atk;
-		int def;
-		bool guard_on;
-		bool dodge_on; 
-		Equipment *equipment; 
-	public:			
-		Unit(string,string); 
-		void showStatus();
-		void newTurn();
-		int attack(Unit &);
-		int ultimateAttack(Unit &); 
-		int beAttacked(int);
-		int heal();	
-		void guard();
-		void dodge(); 
-		bool isDead();
-		void equip(Equipment *);  
-};
-
-Unit::Unit(string t,string n){ 
-	type = t;
-	name = n;
-	if(type == "Hero"){
-		hpmax = rand()%20+120;
-		atk = rand()%5+14;
-		def = rand()%3+9;
-	}else if(t == "Monster"){
-		hpmax = rand()%20+250;
-		atk = rand()%5+25;
-		def = rand()%3+5;
-	}
-	hp = hpmax;
-	dodge_on = false;	
-	guard_on = false;
-	equipment = NULL;
-}
-
-void Unit::showStatus(){
-	if(type == "Hero"){
-		cout << "---------------------------------------\n"; 
-		cout << name << "\n"; 
-		cout << "HP: " << hp << "/" << hpmax << "\tATK: "<< atk << "\t\tDEF: "<< def;		
-		cout << "\n---------------------------------------\n";
-	}	
-	else if(type == "Monster"){
-		cout << "\t\t\t\t---------------------------------------\n"; 
-		cout << "\t\t\t\t" << name << "\n"; 
-		cout << "\t\t\t\tHP: " << hp << "\t\tATK: "<< atk << "\t\tDEF: "<< def;
-		cout << "\n\t\t\t\t---------------------------------------\n";
-	}
-}
-void Unit::dodge(){
-	dodge_on = true;
-}
-
-void Unit::newTurn(){
-	guard_on = false;
-	dodge_on = false; 
-}
-
-int Unit::beAttacked(int oppatk){
-	int dmg;
-	int r = rand()%2;
-	if(oppatk > def){
-		dmg = oppatk-def;	
-		if(guard_on) dmg = dmg/3;
-		if(dodge_on){
-			if(r == 0) dmg = 0;
-			else dmg = dmg * 2;
-			}
-	}	
-	hp -= dmg;
-	if(hp <= 0){hp = 0;}
-	
-	return dmg;	
-}
-
-int Unit::attack(Unit &opp){
-	return opp.beAttacked(atk);
-}
-
-int Unit::ultimateAttack(Unit &opp){
-	return opp.beAttacked(atk*2);
-}
-
-int Unit::heal(){
-	int h = rand()%21 + 10;
-	if(hp + h > hpmax) h = hpmax - hp;
-	hp = hp + h;
-	return h;
-}
-
-void Unit::guard(){
-	guard_on = true;
-}	
-
-bool Unit::isDead(){
-	if(hp <= 0) return true;
-	else return false;
-}
-
-void drawScene(char p_action,int p,char m_action,int m){
-	cout << "                                                       \n";
-	if(p_action == 'A'){
-	cout << "                                       "<< -p <<"\n";
-	}else{
-	cout << "                                                       \n";	
-	}	
-	cout << "                                *               *      \n";
-	cout << "                                **  *********  **      \n";
-	cout << "                                ****         ****      \n";
-	if(m_action == 'A'){
-	cout << "                 " << setw(5) << -m << "           *** **   ** ***       Attack!\n";
-	}else if(m_action == 'G'){
-	cout << "                                 *** **   ** ***       Guard!\n";
-	}else if(m_action == 'D'){
-	cout << "                                 *** **   ** ***       Dodge!\n";
-	}else if(m_action == 'U'){
-	cout << "                 " << setw(5) << -m << "           *** **   ** ***       Ultimate Attack!\n";
-	}else{
-	cout << "                                 *** **   ** ***       \n";	
-	}
-	cout << "                                  ** **   ** **        \n";
-	cout << "                   ***             *         *         \n";
-	if(p_action == 'A'){
-	cout << "        Attack!    ***  *           *********          \n";		
-	}else if(p_action == 'H'){
-	cout << "      Heal! +" << setw(2) << p << "    ***  *           *********          \n";
-	}else if(p_action == 'G'){
-	cout << "         Guard!    ***  *           *********          \n";
-	}else if(p_action == 'D'){
-	cout << "         Dodge!    ***  *           *********          \n";
-	}else{
-	cout << "                   ***  *           *********          \n";	
-	}
-	cout << "                    *  *       ***  *  *  *            \n";
-	cout << "                  *****           **   *   *           \n";
-	cout << "                    *                  *    ***        \n";
-	cout << "                   * *               **        *       \n";
-	cout << "                  *   *                                \n";
-	cout << "                                                       \n";
-};
-
-
-void playerWin(){	
-	cout << "*******************************************************\n";
-	for(int i = 0; i < 3; i++) cout << "*                                                     *\n";
-	cout << "*                   YOU WIN!!!                        *\n";
-	for(int i = 0; i < 3; i++) cout << "*                                                     *\n";
-	cout << "*******************************************************\n";
-};
-
-
-void playerLose(){
-	cout << "*******************************************************\n";
-	cout << "*                                                     *\n";
-	cout << "*                   YOU LOSE!!!                       *\n";
-	cout << "*                                                     *\n";
-	cout << "*******************************************************\n";
-};
-
 
   
 int main(){
@@ -197,18 +22,15 @@ int main(){
 	Equipment shield(0,-1,7);
 	Equipment armor(25,-2,2);
 
-	
-	
 	char eq;	
 	cout << " [1] Sword \n [2] Axes \n [3] Shield \n [4] Armor \n";
 	cout << "Please selet your equipment: ";
 	cin >> eq;
+	
 	if(eq=='1') hero.equip(&sword);
 	else if(eq=='2') hero.equip(&axes);
 	else if(eq=='3') hero.equip(&shield);
-	else if(eq=='4') hero.equip(&armor); 
-
-	
+	else if(eq=='4') hero.equip(&armor);
 	
 	Unit mons("Monster","Kraken");
 	
@@ -250,8 +72,7 @@ int main(){
 			else if(eq=='2') hero.equip(&axes);
 			else if(eq=='3') hero.equip(&shield);
 			else if(eq=='4') hero.equip(&armor);
-
-			
+	
 		}
 		
 		if(player_action == 'A') p = hero.attack(mons); 
